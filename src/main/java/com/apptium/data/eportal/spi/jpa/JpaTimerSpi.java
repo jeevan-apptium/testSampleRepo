246c9af6dc4b594c6b7494de04728002e4a1948e
twallace <twallace@7ee3cdcf-9a02-443e-b632-152f337571ad>
2016-03-16 15:36:10 +0000

    The file based schedule repository refactoring

--- src/main/java/com/apptium/data/eportal/spi/jpa/JpaTimerSpi.java
+++ src/main/java/com/apptium/data/eportal/spi/jpa/JpaTimerSpi.java
@@ -1,13 +1,34 @@
 package com.apptium.data.eportal.spi.jpa;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.UUID;
+import java.util.logging.Logger;
 
+import org.apache.bcel.generic.F2D;
+import org.apache.commons.lang.math.RandomUtils;
+
+import scala.Function1;
+
+import com.apptium.processengine.core.data.dataobjects.TimerBean;
+import com.apptium.processengine.core.data.dataobjects.TimerSPI;
 import com.apptium.serviceproviders.jpa.beans.TimerEntity;
-import com.apptium.serviceproviders.jpa.repos.TimerRepository;
-import com.catify.processengine.core.data.dataobjects.TimerBean;
-import com.catify.processengine.core.data.dataobjects.TimerSPI;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
 /**
  * JPA implementation of the {@link TimerSPI}.
@@ -18,34 +39,121 @@ import com.catify.processengine.core.data.dataobjects.TimerSPI;
  */
 public class JpaTimerSpi extends TimerSPI {
 
-	private TimerRepository timerRepository;
+	private static final Logger LOG = Logger.getLogger(JpaDataObjectSpi.class.getName()); 
+	private static final String SKEDFILE = "bpmnSchedule.jsonTimer"; 
+	
+	private Map <String,TimerEntity> bpmnSked = null; 
+
 	
 	public JpaTimerSpi() {
 		this.implementationId = "jpa-timer"; // set this id in spring
-		//this.timerRepository = (TimerRepository) AppContextFactory.load(TimerRepository.class);
+		if(bpmnSked == null) {
+			bpmnSked = new HashMap<String,TimerEntity>(); 
+			readPersistanceSked(); 
+		}
+		
+	}
+	protected void peristanceSked() throws IOException{
+		Gson gson = new Gson(); 
+		FileWriter f1 = new FileWriter(SKEDFILE,false); 
+		String sked = gson.toJson(bpmnSked);
+		f1.write(sked);
+		f1.flush();
+		f1.close();
+	}
+	
+	
+	protected void readPersistanceSked(){
+		Gson gson = new Gson(); 
+		File fi = new File(SKEDFILE); 
+		if(!fi.exists()) return; 
+		try{
+			JsonParser parser = new JsonParser(); 
+	    Object obj = parser.parse(new FileReader(SKEDFILE)); 
+		JsonObject jsonObject = (JsonObject)obj; 
+		for(Entry<String, JsonElement> test : jsonObject.entrySet()){
+			String key = test.getKey(); 
+			TimerEntity myRec = gson.fromJson(test.getValue(), TimerEntity.class); 
+			bpmnSked.put(key, myRec); 
+		}
+		
+		}catch(Exception ex){
+			ex.printStackTrace();
+			LOG.info(ex.getMessage());
+		}
 	}
 	
 	@Override
 	public void saveTimer(TimerBean timer) {
-		timerRepository.save(new TimerEntity(timer));
+		 try{
+		
+			 TimerEntity myEntity = new TimerEntity(timer); 
+			 myEntity.setId(UUID.randomUUID().toString());
+			 bpmnSked.put(myEntity.getId(), myEntity); 
+			 peristanceSked(); 
+			 
+		 }catch(FileNotFoundException e){
+				e.printStackTrace();
+				LOG.info(e.getMessage());
+		 }catch(Exception e){
+			 e.printStackTrace();
+			 LOG.info(e.getMessage());
+		 }
 	}
 
+
+	
 	@Override
 	public List<TimerBean> loadDueTimers(String actorRef) {
-		List<TimerEntity> timerEntities = timerRepository.findByActorRefAndTimeToFireLessThanTimeNow(actorRef, new Date().getTime());
-		timerRepository.delete(timerEntities);
-		return createTimerBeans(timerEntities);
+		List<TimerBean> result = new ArrayList<TimerBean>(); 
+		List<TimerEntity> timerEntities = new ArrayList<TimerEntity>(); 
+		List<String> expiredSked = new ArrayList<String>(); 
+		long currentTimeStamp = new Date().getTime(); 
+		
+		try{
+			  for(Entry<String, TimerEntity> aRec : bpmnSked.entrySet()){			  
+				  if(aRec.getValue().getActorRef().equalsIgnoreCase(actorRef)){
+					  if(aRec.getValue().getTimeToFire() <= currentTimeStamp){
+						  timerEntities.add(aRec.getValue()); 	
+						  expiredSked.add(aRec.getKey()); 
+							LOG.info(aRec.getValue().getProcessInstanceId()+" >>  "+ aRec.getValue().getTimeToFire()+" <= "+ currentTimeStamp);
+					  }  
+				  }
+				
+			  }	  
+			
+			result = this.createTimerBeans(timerEntities); 
+			for(String target : expiredSked)
+				if(bpmnSked.containsKey(target))bpmnSked.remove(target); 
+			peristanceSked(); 
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+			LOG.info(e.getMessage());
+		}catch(Exception e){
+			e.printStackTrace();
+			LOG.info(e.getMessage());
+		}
+		return result;
 	}
 
+
+	
 	@Override
-	public void deleteTimer(String actorRef, String processInstanceId) {
-		TimerEntity timerEntity = timerRepository.findByActorRefAndProcessInstanceId(actorRef, processInstanceId);
-		// only try to delete timer entity existing timers
-		// this method needs to safely call the delete method for non existing nodes,
-		// because a 'cycle timer start event' will try to delete a trigger that has not been saved to db
-		if (timerEntity != null) {
-			timerRepository.delete(timerEntity);
-		}
+	public void deleteTimer(String actorRef, String processInstanceId){
+		try{
+    		
+    		File file = new File(String.format("%s.jsonTimer",processInstanceId)); 
+    		if(file.delete()){
+    			System.out.println(file.getName() + " is deleted!");
+    		}else{
+    			System.out.println("Delete operation is failed.");
+    		}
+    	   
+    	}catch(Exception e){
+    		
+    		e.printStackTrace();
+    		
+    	}
 	}
 	
 	/**
@@ -62,8 +170,6 @@ public class JpaTimerSpi extends TimerSPI {
 		return timerBeans;
 	}
 
-	public TimerRepository getRepository() {
-		return timerRepository;
-	}
+
 
 }
--- src/test/java/com/apptium/data_eportal_spi/jpa/AppTest.java
+++ src/test/java/com/apptium/data_eportal_spi/jpa/AppTest.java
@@ -1,12 +1,17 @@
 package com.apptium.data_eportal_spi.jpa;
 
+import java.util.List;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.junit.Before;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 
-import com.apptium.data.eportal.spi.jpa.JpaDataObjectSpi;
+import com.apptium.data.eportal.spi.jpa.JpaTimerSpi;
+import com.apptium.processengine.core.data.dataobjects.TimerBean;
 
 /**
  * Unit test for simple App.
@@ -14,6 +19,9 @@ import com.apptium.data.eportal.spi.jpa.JpaDataObjectSpi;
 public class AppTest 
     extends TestCase
 {
+	
+	private static final DateTimeFormatter 	dateFormatter = ISODateTimeFormat.dateTimeNoMillis();
+	
     /**
      * Create the test case
      *
@@ -43,12 +51,43 @@ public class AppTest
    
 	public void testJPADataObjectSPI() {
 
-		JpaDataObjectSpi sp = new JpaDataObjectSpi(); 
+//		JpaDataObjectSpi sp = new JpaDataObjectSpi(); 
+//		
+//		sp.saveObject("uniqueProcessId", "objectId", "instanceId", "dataObject");
+//
+//		assertEquals("dataObject", sp.loadObject("uniqueProcessId", "objectId", "instanceId"));
+//		
+//		sp.deleteObject("uniqueProcessId", "objectId", "instanceId");
+		assertTrue( true );
+	}
+	
+	public void testJPATimerSPI(){
 		
-		sp.saveObject("uniqueProcessId", "objectId", "instanceId", "dataObject");
-
-		assertEquals("dataObject", sp.loadObject("uniqueProcessId", "objectId", "instanceId"));
+		JpaTimerSpi  sp = new JpaTimerSpi(); 
+		DateTime dt = new DateTime(); 
+		String isoDate = dateFormatter.print(dt); 
+		TimerBean timer = new TimerBean(); 
+		timer.setActorRef("actorRef");
+		timer.setProcessInstanceId("processInstance");
+		timer.setTimeToFire(calculateTimeToFireForDate(isoDate));
+		sp.saveTimer(timer);
+		List<TimerBean> myTimer = sp.loadDueTimers("actorRef"); 
+		assertEquals(myTimer.size(), 1); 
+		sp.deleteTimer("actorRef", "processInstance");
 		
-		sp.deleteObject("uniqueProcessId", "objectId", "instanceId");
+	}
+	
+	
+	/**
+	 * Calculates the time stamp in millis for a 
+	 * given ISO 8601 date (e.g. 2013-04-09T16:34:08Z). 
+	 * The date must have a time zone.
+	 * 
+	 * @param isoDate ISO 8601 date as {@link String}
+	 * @return time stamp in millis
+	 */
+	public static long calculateTimeToFireForDate(String isoDate) {
+		DateTime time = dateFormatter.parseDateTime(isoDate);
+		return time.getMillis();
 	}
 }
